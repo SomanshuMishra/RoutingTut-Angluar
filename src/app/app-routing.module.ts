b022bbcb8ca997d6911aefb55a4cc574908357ca
SomanshuMishra <mishrasomanshu@gmail.com>
2021-11-08 17:45:30 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {CourseComponent} from './components/course/course.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {BlogComponent} from './components/blog/blog.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'blog',
+    component:BlogComponent
+  },
+  {
+    path:'course',
+    component:CourseComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
